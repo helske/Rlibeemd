2cfc0f0b91b9f4c9d6ac339820b00319b384137b
Jouni Helske <jouni.helske@jyu.fi>
2017-06-16 12:54:20 +0100

    added float data and example

--- R/bemd.R
+++ R/bemd.R
@@ -5,7 +5,8 @@
 #' @export
 #' @name bemd
 #' @param input Complex vector of length N. The input signal to decompose.
-#' @param directions Vector of directions (phi_k in the article) used for decomposition.
+#' @param directions Vector of directional angles (in radians) to use for the decomposition, 
+#' or an integer defining the number of equally spaced angles to use.
 #' @param num_imfs Number of Intrinsic Mode Functions (IMFs) to compute. If num_imfs is set to zero, a value of
 #'        num_imfs = emd_num_imfs(N) will be used, which corresponds to a maximal number of
 #'        IMFs. Note that the final residual is also counted as an IMF in this
@@ -28,13 +29,32 @@
 #' # Use evenly spaced angles as directions
 #' num_directions <- 64
 #' directions <- 2 * pi * 1:num_directions / num_directions
-#' output <- bemd(input, directions, num_imfs = 4, num_siftings = 10)
+#' imfs <- bemd(input, directions, num_imfs = 4, num_siftings = 10)
+#' 
+#' # plot the data
 #' plot(Re(input), Im(input), xlim = c(-1, 2))
+#' # plot signal and the imfs
 #' for(i in 1:4)
-#'   points(Re(output[,i]), Im(output[,i]), col = 1 + i)
+#'   points(Re(imfs[,i]), Im(imfs[,i]), col = 1 + i)
 #' legend("bottomright", col = 1:5, legend = c("signal", paste0("IMF ",1:4)), pch = 1)
 #' 
-bemd <- function(input, directions, num_imfs = 0, num_siftings = 50L) {
+#' data("float")
+#' plot(float, type = "l")
+#' signal <- float[, 1] + float[, 2] * 1i
+#' imfs <- bemd(signal, num_siftings = 10, num_imfs = 4)
+#' 
+#' # plot the data and the imfs
+#' oldpar <- par()
+#' par(mfrow = c(5, 1), mar = c(0.5, 4.5, 0.5, 0.5), oma = c(4, 0, 2, 0))
+#' ts.plot(float, col = 1:2, lty = 1:2, ylab = "signal", gpars = list(xaxt = "n"))
+#' for(i in 1:4) {
+#'   ts.plot(Re(imfs[, i]), Im(imfs[, i]), col = 1:2, lty = 1:2, 
+#'     ylab = if(i < 4) paste("IMF", i) else "residual", gpars = list(xaxt = "n"))
+#'  }
+#' axis(1)
+#' title(xlab = "Time (days)", main = "Bivariate EMD decomposition", outer = TRUE)
+#' 
+bemd <- function(input, directions = 64L, num_imfs = 0L, num_siftings = 50L) {
   if (!all(is.finite(input))) 
     stop("'input' must contain finite values only.")
   if (num_imfs < 0)
@@ -43,7 +63,9 @@ bemd <- function(input, directions, num_imfs = 0, num_siftings = 50L) {
     stop("Argument 'num_siftings' must be non-negative integer.")
   if (!all(is.finite(directions))) 
     stop("'input' must contain finite values only.")
-  
+  if (length(directions) == 1) {
+    directions <- 2 * pi * 0:(directions - 1) / directions
+  }
   output <- bemdR(input, directions,num_imfs, num_siftings)
   if (inherits(input, "ts")) {
     tsp(output) <- tsp(input)
--- R/ecg.R
+++ R/ecg.R
@@ -13,4 +13,24 @@
 #' data(ECG)
 #' plot(ECG)
 NULL
-
+#'
+#'  Float Data
+#'  
+#'  The data are a position record from an acoustically tracked subsurface oceanographic float, 
+#'  used as an example data in Rilling et al (2007).
+#'  
+#' @name float
+#' @docType data
+#' @format A time series object.
+#' @source  http://wfdac.whoi.edu
+#' @keywords datasets
+#'  @references
+#' \enumerate{
+#'       \item{G. Rilling, P. Flandrin, P. Goncalves and J. M. Lilly,
+#'   "Bivariate Empirical Mode Decomposition",
+#'   IEEE Signal Processing Letters, Vol. 14 (2007) 936--939}
+#'       }
+#' @examples
+#' data(float)
+#' plot(float, type = "l"))
+NULL
