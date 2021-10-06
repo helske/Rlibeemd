4e5d0e3afe54c8b08027f82384efe13c57b54b94
helske <jouni.helske@iki.fi>
2021-10-06 16:35:57 +0300

    ascii character

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@
     "devtools::install_github('helske/Rlibeemd')")
   
   citation_note <- paste0("\nPlease cite Rlibeemd in publications by using: \n",
-    "Luukko PJ, Helske J, Räsänen E (2016). Introducing libeemd:", 
+    "Luukko PJ, Helske J, R\U00E4s\U00E4nen E (2016). Introducing libeemd:", 
     "A program package for performing the ensemble empirical mode decomposition.",
     "Computational Statistics, 31(2), 545–557. doi: 10.1007/s00180-015-0603-9.")
   
